9834d1ea533bca45fedc112c1fe0565c26ea10b1
waterlow <waterlow3442@gmail.com>
2023-01-02 08:45:31 +0900

    rubocop -a

--- Gemfile
+++ Gemfile
@@ -1,29 +1,31 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.1.3"
+ruby '3.1.3'
 
-gem "bootsnap", require: false
-gem "importmap-rails"
-gem "jbuilder"
-gem "pg", "~> 1.1"
-gem "puma", "~> 5.0"
-gem "rails", "~> 7.0.4"
-gem "sprockets-rails"
-gem "stimulus-rails"
-gem "turbo-rails"
+gem 'bootsnap', require: false
+gem 'importmap-rails'
+gem 'jbuilder'
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 5.0'
+gem 'rails', '~> 7.0.4'
+gem 'sprockets-rails'
+gem 'stimulus-rails'
+gem 'turbo-rails'
 
 group :development, :test do
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
-  gem "rspec-rails"
+  gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails'
 end
 
 group :development do
-  gem "web-console"
+  gem 'web-console'
 end
 
 group :test do
-  gem "capybara"
-  gem "selenium-webdriver"
-  gem "webdrivers"
+  gem 'capybara'
+  gem 'selenium-webdriver'
+  gem 'webdrivers'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
+RSpec.describe 'StaticPages', type: :request do
   base_title = 'Ruby on Rails Tutorial Sample App'
 
-  describe "GET /" do
-    it "should get home" do
+  describe 'GET /' do
+    it 'should get home' do
       get root_path
       expect(response).to have_http_status(200)
       expect(css_select('title').text).to eq("Home | #{base_title}")
     end
   end
 
-  describe "GET /static_pages/help" do
-    it "should get help" do
+  describe 'GET /static_pages/help' do
+    it 'should get help' do
       get static_pages_help_path
       expect(response).to have_http_status(200)
       expect(css_select('title').text).to eq("Help | #{base_title}")
     end
   end
 
-  describe "GET /static_pages/about" do
-    it "should get about" do
+  describe 'GET /static_pages/about' do
+    it 'should get about' do
       get static_pages_about_path
       expect(response).to have_http_status(200)
       expect(css_select('title').text).to eq("About | #{base_title}")
     end
   end
 
-  describe "GET /static_pages/contact" do
-    it "should get contact" do
+  describe 'GET /static_pages/contact' do
+    it 'should get contact' do
       get static_pages_contact_path
       expect(response).to have_http_status(200)
       expect(css_select('title').text).to eq("Contact | #{base_title}")
