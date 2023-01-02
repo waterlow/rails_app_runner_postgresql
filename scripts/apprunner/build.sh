5b8071a11a18847bfbe931e7978a3e908815087f
waterlow <waterlow3442@gmail.com>
2023-01-02 09:27:13 +0900

    不要ファイル削除

--- scripts/apprunner/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-amazon-linux-extras install -y postgresql14
-yum install -y postgresql-devel
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
--- scripts/run_command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bin/rails db:migrate
-bundle exec puma -t 5:5 -p 3000
