ecd3ec76d96dea4c8f0c6bf5a022ad028e3d0b4e
waterlow <waterlow3442@gmail.com>
2023-01-08 15:09:58 +0900

    予めrpmから落としておく必要あり

--- scripts/apprunner/build.sh
+++ scripts/apprunner/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 amazon-linux-extras install -y postgresql14
+curl -fsSL https://rpm.nodesource.com/setup_16.x | bash -
 yum install -y postgresql-devel nodejs
 export RAILS_ENV=production
 export BUNDLE_WITHOUT=development:test
