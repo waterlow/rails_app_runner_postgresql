7d9802f6f9304469c5c0261d5f67695a2f6cb8a6
waterlow <waterlow3442@gmail.com>
2023-01-08 15:26:59 +0900

    dummy_secret

--- scripts/apprunner/build.sh
+++ scripts/apprunner/build.sh
@@ -5,7 +5,7 @@ curl -fsSL https://rpm.nodesource.com/setup_16.x | bash -
 yum install -y postgresql-devel nodejs
 export RAILS_ENV=production
 export BUNDLE_WITHOUT=development:test
-export SECRET_KEY_BASE=
+export SECRET_KEY_BASE=dummy
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
