42e9b45e4f4ed283f730550b5ec9d40e51cc150c
waterlow <waterlow3442@gmail.com>
2023-01-08 15:19:56 +0900

    wip

--- scripts/apprunner/build.sh
+++ scripts/apprunner/build.sh
@@ -5,6 +5,7 @@ curl -fsSL https://rpm.nodesource.com/setup_16.x | bash -
 yum install -y postgresql-devel nodejs
 export RAILS_ENV=production
 export BUNDLE_WITHOUT=development:test
+export SECRET_KEY_BASE=
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
