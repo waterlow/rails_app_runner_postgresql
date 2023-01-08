cf9f81479b452215de631f188e8ff5d7720212c8
waterlow <waterlow3442@gmail.com>
2023-01-08 14:48:10 +0900

    node必要

--- scripts/apprunner/build.sh
+++ scripts/apprunner/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
 export RAILS_ENV=production
 export BUNDLE_WITHOUT=development:test
-amazon-linux-extras install -y postgresql14
-yum install -y postgresql-devel
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
