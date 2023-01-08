437fce4bdb5a0e615aa5a04737422f9c26c3711f
waterlow <waterlow3442@gmail.com>
2023-01-08 14:53:30 +0900

    postgresライブラリのインストールをスキップしていた

--- scripts/apprunner/build.sh
+++ scripts/apprunner/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+amazon-linux-extras install -y postgresql14
+yum install -y postgresql-devel
 export RAILS_ENV=production
 export BUNDLE_WITHOUT=development:test
 bundle install
