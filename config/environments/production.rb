4eb13a04a3a06f42185d9c536bf1fa174a2e7b11
waterlow <waterlow3442@gmail.com>
2023-01-13 21:47:57 +0900

    sessionの管理はredisを使う

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.session_store(
+    :redis_store,
+    servers: [ENV['REDIS_URL']],
+    expire_after: 90.minutes,
+    key: "_#{Rails.application.class.module_parent_name.downcase}_session",
+  )
 end
